e11fd2fe1b5a0c6932fbc45eeff4a405057489f6
sumita12345 <sumitamsomankatti@gmail.com>
2025-04-09 09:55:42 +0530

    save

---
+++ substraction.java
@@ -0,0 +1,12 @@
+package java1;
+
+public class substraction { 
+        public static void main(String[] args) {
+            int a,b;
+            a=10;
+            b=5;
+            int sub=a-b;
+            System.out.println("Substraction of a & b is:"+sub);
+    
+        }
+    }
