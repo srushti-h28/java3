9de9d8b777e2f3ee4602cdabb9dd48f2ede072ba
sumita12345 <sumitamsomankatti@gmail.com>
2025-04-09 09:44:47 +0530

    save

---
+++ Addition.java
@@ -0,0 +1,16 @@
+package java1;
+
+public class Addition {
+        public static void main(String[] args) {
+            int a,b;
+            a=26;
+            b=5;
+            int sum=a+b;
+            System.out.println("Addition of a & b is:"+sum);
+    
+        }
+    }
+            
+    
+    
+
